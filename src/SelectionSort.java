2d4084c14c3c0f375ed0926c5ebef1272db246b8
fxx <fanxx_3516@163.com>
2018-11-28 14:46:22 +0800

    选择排序java实现

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -2,12 +2,41 @@
  * @ProjectName: algorithm
  * @Package: PACKAGE_NAME
  * @ClassName: ${TYPE_NAME}
- * @Description:
+ * @Description: 将数组中的元素, 从小到大进行排序.
  * @Author: fanxx
  * @CreateDate: 2018/11/20 17:47
  * <p>Copyright: Copyright (c) 2018</p>
  */
 public class SelectionSort {
+    //todo: 1.寻找到[i, n) 区间内的最小值
+    //todo: 2.将最小值, 和排序位置的元素进行交换 (C++使用swap函数进行交换, java用什么?)
+    //todo: 3.测试排序算法
+
+    public void selectionSortMethod(int[] arr, int n){ //arr是数组, n是数组元素个数
+
+        for (int i = 0; i < n; i++){
+
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++){
+                if(arr[j] <= arr[minIndex]){
+                    int a = arr[j];
+                    arr[j] = arr[minIndex];
+                    arr[minIndex] = a;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 6, 9, 4, 0, 1, 5, 7, 8};
+        SelectionSort ss = new SelectionSort();
+        ss.selectionSortMethod(arr, 10);
+
+        for(int i = 0; i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
 
     
 }
