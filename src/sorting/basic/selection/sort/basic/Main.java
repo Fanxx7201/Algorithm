a3a14021a65b5a25c93cf785d8c19033e42fe424
fxx <fanxx_3516@163.com>
2018-12-11 10:24:58 +0800

    整理目录结构

--- src/sorting/basic/selection/sort/basic/Main.java
+++ src/sorting/basic/selection/sort/basic/Main.java
@@ -1,3 +1,5 @@
+package sorting.basic.selection.sort.basic;
+
 public class Main {
 
     public static void main(String[] args) {
--- src/sorting/basic/selection/sort/basic/SelectionSort.java
+++ src/sorting/basic/selection/sort/basic/SelectionSort.java
@@ -1,8 +1,10 @@
+package sorting.basic.selection.sort.basic;
+
 /**
  * @ProjectName: algorithm
  * @Package: PACKAGE_NAME
  * @ClassName: ${TYPE_NAME}
- * @Description: 将数组中的元素, 从小到大进行排序.
+ * @Description: 排序算法
  * @Author: fanxx
  * @CreateDate: 2018/11/20 17:47
  * <p>Copyright: Copyright (c) 2018</p>
@@ -11,10 +13,12 @@ public class SelectionSort {
     //todo: 1.寻找到[i, n) 区间内的最小值
     //todo: 2.将最小值, 和排序位置的元素进行交换 (C++使用swap函数进行交换, java用什么? 用自定义的swap方法就行)
     //todo: 3.测试排序算法
-    //
 
-    public void selectionSortMethod(int[] arr, int n){ //arr是数组, n是数组元素个数
+    //算法不允许产生实例
+    private SelectionSort(){}
 
+    public static void selectionSortMethod(int[] arr){ //arr是数组, n是数组元素个数
+        int n = arr.length;
         for (int i = 0; i < n; i++){
 
             int minIndex = i;
@@ -30,10 +34,11 @@ public class SelectionSort {
 
     public static void main(String[] args) {
         int[] arr = {2, 3, 6, 9, 4, 0, 1, 5, 7, 8};
-        SelectionSort ss = new SelectionSort();
-        ss.selectionSortMethod(arr, 10);
-
-        for(int i = 0; i < arr.length;i++){
+        //算法不允许产生实例
+        //SelectionSort ss = new SelectionSort();
+        //ss.selectionSortMethod(arr, 10);
+        SelectionSort.selectionSortMethod(arr);
+        for(int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
         }
     }
