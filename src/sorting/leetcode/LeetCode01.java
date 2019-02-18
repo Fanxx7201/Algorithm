c4e538531986a5b74d6501f876129c1de4f73747
fxx <fanxx_3516@163.com>
2019-02-18 14:07:51 +0800

    两数求和

---
+++ src/sorting/leetcode/LeetCode01.java
@@ -0,0 +1,42 @@
+package sorting.leetcode;
+
+/**
+ * @ProjectName: algorithm
+ * @Package: sorting.leetcode
+ * @Description:
+ * @Author: fanxx
+ * @CreateDate: 2019/2/18 13:36
+ * <p>Copyright: Copyright (c) 2019</p>
+ */
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ **/
+public class LeetCode01 {
+
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] ints = twoSum(nums, target);
+        System.out.println(ints[0]);
+        System.out.println(ints[1]);
+    }
+
+
+    private static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
