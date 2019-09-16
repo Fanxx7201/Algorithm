a68414805dad953ecf8f02396c9158a6a0c716fc
fxx <fanxx_3516@163.com>
2019-09-16 13:53:20 +0800

    isPowerOfTwo

--- src/sorting/leetcode/isPowerOfTwo.java
+++ src/sorting/leetcode/isPowerOfTwo.java
@@ -46,6 +46,20 @@ public class isPowerOfTwo {
 
     }
 
+
+    /*
+     若 n = 2^xn=2
+        x
+          且 xx 为自然数（即 nn 为 22 的幂），则一定满足以下条件：
+        恒有 n & (n - 1) == 0，这是因为：
+        nn 二进制最高位为 11，其余所有位为 00；
+        n - 1n−1 二进制最高位为 00，其余所有位为 11；
+        一定满足 n > 0。
+        因此，通过 n > 0 且 n & (n - 1) == 0 即可判定是否满足 n = 2^xn=2
+        x
+     **/
+    //return n > 0 && (n & (n - 1)) == 0;
+
     public static void main(String[] args) {
         System.out.println(isPowerOfTwo(218));
     }
