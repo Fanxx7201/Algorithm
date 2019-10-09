610c90d824aa2938ecd116e439e6bbacb023581d
fxx <fanxx_3516@163.com>
2019-10-09 09:28:28 +0800

    removeDuplicates

--- src/sorting/leetcode/RemoveDuplicates.java
+++ src/sorting/leetcode/RemoveDuplicates.java
@@ -10,13 +10,13 @@ package sorting.leetcode;
  */
 public class RemoveDuplicates {
 
-    public static int removeEuplicates(int[] nums){
+    public static int removeEuplicates1(int[] nums){
         int len = nums.length;
         int i = 0;
         while(i < len - 1){
             if(nums[i] == nums[i + 1]){
-                for(int j = i; j < len -1; j++){
-                    nums[j] = nums[j + i];
+                for(int j = i; j < len - 1; j++){
+                    nums[j] = nums[j + 1];
                 }
                 len --;
             }else{
@@ -24,5 +24,23 @@ public class RemoveDuplicates {
             }
         }
         return len;
+    }
+
+
+
+    public static int removeEuplicates2(int[] nums){
+        int i = 0;
+        for(int j = 1; j < nums.length; j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(removeEuplicates1(new int[] {0, 0, 1, 2, 3, 3, 3, 4, 4, 5, 5, 5}));
     }
 }
