e1e8c3b0133d9472c589246838d0278e5d41d232
fxx <fanxx_3516@163.com>
2019-10-17 14:38:37 +0800

    addTwoNumbers

---
+++ src/sorting/leetcode/ListNode2.java
@@ -0,0 +1,22 @@
+package sorting.leetcode;
+
+/**
+ * @ProjectName: algorithm
+ * @Package: sorting.leetcode
+ * @Description:
+ * @Author: fanxx
+ * @CreateDate: 2019/10/17 11:04
+ * <p>Copyright: Copyright (c) 2019</p>
+ */
+public class ListNode2 {
+
+    int value;
+    ListNode2 next;
+
+    ListNode2(int value){
+        this.value = value;
+    }
+    ListNode2(){
+
+    }
+}
